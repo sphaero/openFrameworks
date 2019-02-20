dbfe46c01c986ca353d87bfe9766a1db5a05c5d8
Arnaud Loonstra <arnaud@sphaero.org>
2019-02-20 13:56:48 +0100

    no console on windows for release targets

--- libs/openFrameworksCompiled/project/msys2/config.msys2.default.mk
+++ libs/openFrameworksCompiled/project/msys2/config.msys2.default.mk
@@ -33,6 +33,9 @@ MSYS2_ROOT = /mingw32
 PLATFORM_CFLAGS += -std=gnu++14 -DUNICODE -D_UNICODE
 #PLATFORM_CFLAGS += -IC:/msys64/mingw32/include/gstreamer-1.0 -DOF_VIDEO_PLAYER_GSTREAMER
 PLATFORM_LDFLAGS += -lpthread
+ifndef DEBUG
+	PLATFORM_LDFLAGS += -mwindows
+endif
 #ifeq ($(PLATFORM_ARCH),x86_64)
 ifdef USE_CCACHE
 	CC = ccache $(MSYS2_ROOT)/bin/gcc
@@ -284,6 +287,8 @@ PLATFORM_LIBRARY_SEARCH_PATHS =
 #    Don't want to use a default compiler?
 ################################################################################
 #PLATFORM_CC=
+
+OF_PROJECT_SOURCE_FILES += "icon.rc"
 
 copy_dlls:
 	@echo "     copying dlls to bin"
